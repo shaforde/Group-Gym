15433a810b597597c45e86c4bfc060ccf3767d14
stadmin <stadmin@nyc.yearup.org>
2018-11-26 14:55:10 -0500

    added event listener

---
+++ main.js
@@ -0,0 +1,14 @@
+let poketrainer=document.getElementById('Shanelle');
+poketrainer.addEventListener("click",)
+
+let poketrainer=document.getElementById('Dekwondra');
+poketrainer.addEventListener("click",)
+
+let poketrainer=document.getElementById('Kendra');
+poketrainer.addEventListener("click",)
+
+let poketrainer=document.getElementById('Rajin');
+poketrainer.addEventListener("click",)
+
+let poketrainer=document.getElementById('Rinzen');
+poketrainer.addEventListener("click",)
